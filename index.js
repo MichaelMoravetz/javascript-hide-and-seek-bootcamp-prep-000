b6951c7440ca8395294f66fde35dd486a4450597
Michael Moravetz <moravemv@gmail.com>
2018-12-13 22:42:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+function getFirstSelector(selector) {
+ 
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  
+  return document.getElementsByTagName('div')[4] 
+}
+ 
+ function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
+
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
+  }
+
+// function deepestChild() {
+  
+//   var deepChild = document.getElementById('grand-node').querySelectorAll('div div div div');
+ 
+// return deepChild[0].querySelector('div')
+  // // for (let i = 0; i <= deepChild.length; i++) {
+    
+  // //   return deepChild[i].querySelector('div')
+  // }
+// }
+ 
+ function increaseRankBy(n) {
+   
+   var lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+   
+   for (let i = 0; i < lis.length; i++) {
+     
+     lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+    
+   }
+ }
