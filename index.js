92dff0f45ddd780dd2c27a005552d95609eaa198
Michael Moravetz <moravemv@gmail.com>
2018-12-13 21:18:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function getFirstSelector(selector) {
+ 
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  
+  return document.getElementsByTagName('div')[4] 
+}
+ 
+ function deepestChild() {
+    return document.getElementsByTagName('div').getElementById(grand-node)
+ }
